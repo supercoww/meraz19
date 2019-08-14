7d66589cc89b3c13910f5a0b8e08ece7323e71c7
ambarytl <ambar.ytl@gmail.com>
2019-08-15 00:41:04 +0530

    add hero component and sticky top-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
-	declarations: [AppComponent, HomeComponent, TopBarComponent],
+	declarations: [AppComponent, HomeComponent, TopBarComponent, HeroComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent]
