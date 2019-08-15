8b225c4e0dae5d2f716652bf37dff0652d6d4033
ambarytl <ambar.ytl@gmail.com>
2019-08-15 18:26:44 +0530

    add timer component and change font (probably temporary)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { HeroComponent } from './hero/hero.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
-	declarations: [AppComponent, HomeComponent, TopBarComponent, HeroComponent],
+	declarations: [AppComponent, HomeComponent, TopBarComponent, HeroComponent, TimerComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent]
---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Time } from '../interfaces';
+
+const END_TIME: number = new Date('Nov 8, 2019 00:00:00').getTime(); // End time of timer
+const TIMER_INTERVAL = 1000;
+
+@Component({
+	selector: 'app-timer',
+	templateUrl: './timer.component.html',
+	styleUrls: ['./timer.component.scss']
+})
+export class TimerComponent implements OnInit {
+	remainingTimeSubject: BehaviorSubject<Time>;
+	interval: NodeJS.Timer;
+
+	constructor() {
+		this.remainingTimeSubject = new BehaviorSubject(this.timeLeft);
+	}
+
+	ngOnInit() {
+		this.interval = setInterval(() => {
+			const timeLeft = this.timeLeft;
+			this.remainingTimeSubject.next(timeLeft);
+		}, TIMER_INTERVAL);
+	}
+
+	public get timeLeft(): Time {
+		const totalSeconds = END_TIME - Date.now();
+		if (totalSeconds === 0) {
+			clearInterval(this.interval);
+		}
+		return {
+			days: Math.floor(totalSeconds / (1000 * 60 * 60 * 24)),
+			hours: Math.floor((totalSeconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+			minutes: Math.floor((totalSeconds % (1000 * 60 * 60)) / (1000 * 60)),
+			seconds: Math.floor((totalSeconds % (1000 * 60)) / 1000)
+		};
+	}
+}
