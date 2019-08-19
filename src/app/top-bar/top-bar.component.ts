139fd1dc20b5be8c1f7703b9aeb962f4e0a3f69d
ambarytl <ambar.ytl@gmail.com>
2019-08-19 11:10:45 +0530

    move top-bar to top add variable bg color to top-bar

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-top-bar',
-  templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.scss']
+	selector: 'app-top-bar',
+	templateUrl: './top-bar.component.html',
+	styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent implements OnInit {
+	bgColor: string;
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		window.addEventListener('scroll', (event: Event) => {
+			if (window.scrollY > 56) {
+				this.bgColor = 'rgba(0,0,0,0.8)';
+			} else {
+				this.bgColor = 'rgba(0,0,0,0)';
+			}
+		});
+	}
 }
