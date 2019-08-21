bf7731d8e6ef2e694dfa29f994abef9c4a4a1ae2
ambarytl <ambar.ytl@gmail.com>
2019-08-22 01:09:51 +0530

    change hero background

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -9,8 +9,8 @@ import { filter } from 'rxjs/operators';
 })
 export class TopBarComponent implements OnInit {
 	bgColor: string;
-	fgColor: string;
 	transparent: boolean;
+	shadow: string;
 
 	constructor(private router: Router) {
 		router.events
@@ -25,10 +25,11 @@ export class TopBarComponent implements OnInit {
 		window.addEventListener('scroll', (event: Event) => {
 			if (window.scrollY > 56 || !this.transparent) {
 				this.bgColor = 'white';
-				this.fgColor = 'black';
+				this.shadow =
+					'0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2)';
 			} else {
 				this.bgColor = 'rgba(0,0,0,0)';
-				this.fgColor = 'white';
+				this.shadow = 'none';
 			}
 		});
 	}
