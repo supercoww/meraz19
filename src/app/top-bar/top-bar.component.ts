7c117181113d9bc2fb5b844216ac7390b9001b19
ambarytl <ambar.ytl@gmail.com>
2019-08-22 12:37:14 +0530

    spread 🌑darkness with sass variables

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -24,7 +24,7 @@ export class TopBarComponent implements OnInit {
 	ngOnInit() {
 		window.addEventListener('scroll', (event: Event) => {
 			if (window.scrollY > 56 || !this.transparent) {
-				this.bgColor = 'white';
+				this.bgColor = 'black';
 				this.shadow =
 					'0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2)';
 			} else {
