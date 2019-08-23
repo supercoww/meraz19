1defa534815ad7754fe0b36612948ea26b3ce3c0
KShivendu <shivendu@iitbhilai.ac.in>
2019-08-23 12:08:20 +0530

    added animated event elements

--- src/app/pages/event-list/event-list.component.ts
+++ src/app/pages/event-list/event-list.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { EventDataService } from 'src/app/core/event-data.service';
-import { Category } from 'src/interfaces';
+import { Component, OnInit } from "@angular/core";
+import { EventDataService } from "src/app/core/event-data.service";
+import { Category } from "src/interfaces";
+import { init } from "src/app/pages/event-list/hover-move";
 
 @Component({
-	selector: 'app-event-list',
-	templateUrl: './event-list.component.html',
-	styleUrls: ['./event-list.component.scss']
+	selector: "app-event-list",
+	templateUrl: "./event-list.component.html",
+	styleUrls: ["./event-list.component.scss", "./hover.css"]
 })
 export class EventListComponent implements OnInit {
 	categories: Category[];
@@ -14,9 +15,12 @@ export class EventListComponent implements OnInit {
 
 	ngOnInit() {
 		this.getAllEvents();
+		init();
 	}
 
 	getAllEvents() {
-		this.eventDataService.getAllEvents().subscribe(categories => (this.categories = categories));
+		this.eventDataService
+			.getAllEvents()
+			.subscribe(categories => (this.categories = categories));
 	}
 }
