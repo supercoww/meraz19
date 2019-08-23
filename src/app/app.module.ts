351c851dc48b7c863609ca874782daf2a6c77f73
KShivendu <shivendu@iitbhilai.ac.in>
2019-08-23 21:59:29 +0530

    Added particle js for angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { HeroComponent } from './hero/hero.component';
-import { TimerComponent } from './timer/timer.component';
-import { FooterComponent } from './footer/footer.component';
-import { EventListComponent } from './pages/event-list/event-list.component';
-import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { HeroComponent } from "./hero/hero.component";
+import { TimerComponent } from "./timer/timer.component";
+import { FooterComponent } from "./footer/footer.component";
+import { EventListComponent } from "./pages/event-list/event-list.component";
+import { CoreModule } from "./core/core.module";
+import { ParticlesModule } from "angular-particle";
 
 @NgModule({
 	declarations: [
@@ -22,7 +23,13 @@ import { CoreModule } from './core/core.module';
 		FooterComponent,
 		EventListComponent
 	],
-	imports: [BrowserModule, CoreModule, HttpClientModule, AppRoutingModule],
+	imports: [
+		BrowserModule,
+		CoreModule,
+		HttpClientModule,
+		AppRoutingModule,
+		ParticlesModule
+	],
 	providers: [],
 	bootstrap: [AppComponent]
 })
