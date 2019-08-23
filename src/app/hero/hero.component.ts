c8bbd8d7fa35de16dcceb9666086e562c0fcde75
KShivendu <shivendu@iitbhilai.ac.in>
2019-08-23 19:28:40 +0530

    Added font and particles in hero

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { particles } from "particles.js";
 @Component({
-	selector: 'app-hero',
-	templateUrl: './hero.component.html',
-	styleUrls: ['./hero.component.scss']
+	selector: "app-hero",
+	templateUrl: "./hero.component.html",
+	styleUrls: ["./hero.component.scss", "./particles.scss"]
 })
 export class HeroComponent implements OnInit {
 	constructor() {}
