378e8eb36d5ad4fc8097862a940d1dc0cd8b07d3
KShivendu <shivendu@iitbhilai.ac.in>
2019-08-24 19:23:02 +0530

    Changed some more looks

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -29,109 +29,68 @@ export class HeroComponent implements OnInit {
 		this.myParams = {
 			particles: {
 				number: {
-					value: 50,
-					density: {
-						enable: true,
-						value_area: 800
-					}
+					value: 400,
+					density: { enable: true, value_area: 3000 }
 				},
-				color: {
-					value: "#dc5811"
-				},
+				color: { value: "#fc0000" },
 				shape: {
 					type: "circle",
-					stroke: {
-						width: 0,
-						color: "#000000"
-					},
-					polygon: {
-						nb_sides: 5
-					},
-					image: {
-						src: "img/github.svg",
-						width: 100,
-						height: 100
-					}
+					stroke: { width: 0, color: "#000000" },
+					polygon: { nb_sides: 3 },
+					image: { src: "img/github.svg", width: 100, height: 100 }
 				},
 				opacity: {
-					value: 1,
+					value: 0.5,
 					random: true,
 					anim: {
-						enable: true,
+						enable: false,
 						speed: 1,
-						opacity_min: 0,
+						opacity_min: 0.1,
 						sync: false
 					}
 				},
 				size: {
-					value: 3,
+					value: 2,
 					random: true,
-					anim: {
-						enable: false,
-						speed: 4,
-						size_min: 0.3,
-						sync: false
-					}
+					anim: { enable: true, speed: 5, size_min: 31, sync: false }
 				},
 				line_linked: {
 					enable: false,
-					distance: 150,
+					distance: 500,
 					color: "#ffffff",
 					opacity: 0.4,
-					width: 1
+					width: 2
 				},
 				move: {
 					enable: true,
-					speed: 1,
-					direction: "none",
+					speed: 7.8914764163227265,
+					direction: "top-left",
 					random: true,
 					straight: false,
 					out_mode: "out",
 					bounce: false,
-					attract: {
-						enable: false,
-						rotateX: 600,
-						rotateY: 600
-					}
+					attract: { enable: false, rotateX: 600, rotateY: 1200 }
 				}
 			},
 			interactivity: {
 				detect_on: "canvas",
 				events: {
-					onhover: {
-						enable: true,
-						mode: "bubble"
-					},
-					onclick: {
-						enable: true,
-						mode: "repulse"
-					},
+					onhover: { enable: false, mode: "bubble" },
+					onclick: { enable: false, mode: "repulse" },
 					resize: true
 				},
 				modes: {
-					grab: {
-						distance: 400,
-						line_linked: {
-							opacity: 1
-						}
-					},
+					grab: { distance: 400, line_linked: { opacity: 0.5 } },
 					bubble: {
-						distance: 250,
-						size: 0,
-						duration: 2,
-						opacity: 0,
+						distance: 400,
+						size: 4,
+						duration: 0.3,
+						opacity: 1,
 						speed: 3
 					},
-					repulse: {
-						distance: 400,
-						duration: 0.4
-					},
-					push: {
-						particles_nb: 4
-					},
-					remove: {
-						particles_nb: 2
-					}
+					repulse: { distance: 200, duration: 0.4 },
+					push: { particles_nb: 4 },
+					remove: { particles_nb: 2 }
 				}
 			},
 			retina_detect: true
