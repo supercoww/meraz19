cb4c3fb0af89cb4a56a627332dce194904d63921
KShivendu <shivendu@iitbhilai.ac.in>
2019-09-07 21:24:59 +0530

    Rectified register form and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { HomeComponent } from "./pages/home/home.component";
-import { TopBarComponent } from "./top-bar/top-bar.component";
-import { HeroComponent } from "./hero/hero.component";
-import { TimerComponent } from "./timer/timer.component";
-import { FooterComponent } from "./footer/footer.component";
-import { EventListComponent } from "./pages/event-list/event-list.component";
-import { CoreModule } from "./core/core.module";
-import { ParticlesModule } from "angular-particle";
-import { EventComponent } from "./pages/event/event.component";
-import { SidebarComponent } from "./sidebar/sidebar.component";
-import { NoopAnimationsModule } from "@angular/platform-browser/animations";
-import { RegisterFormComponent } from "./pages/register-form/register-form.component";
-import { MatButtonModule } from "@angular/material/button";
-import {MatInputModule} from '@angular/material/input';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { HeroComponent } from './hero/hero.component';
+import { TimerComponent } from './timer/timer.component';
+import { FooterComponent } from './footer/footer.component';
+import { EventListComponent } from './pages/event-list/event-list.component';
+import { CoreModule } from './core/core.module';
+import { ParticlesModule } from 'angular-particle';
+import { EventComponent } from './pages/event/event.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterFormComponent } from './pages/register-form/register-form.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
 	declarations: [
@@ -40,7 +42,8 @@ import {MatInputModule} from '@angular/material/input';
 		ParticlesModule,
 		NoopAnimationsModule,
 		MatButtonModule,
-		MatInputModule
+		MatInputModule,
+		BrowserAnimationsModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
