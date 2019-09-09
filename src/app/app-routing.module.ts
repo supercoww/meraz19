78f56528c7b726be456a9594051210dc45cb83eb
supercoww <ambar.ytl@gmail.com>
2019-09-09 13:14:51 +0530

    create contact component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { HomeComponent } from "./pages/home/home.component";
-import { EventListComponent } from "./pages/event-list/event-list.component";
-import { EventComponent } from "./pages/event/event.component";
-import { RegisterFormComponent } from "./pages/register-form/register-form.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { EventListComponent } from './pages/event-list/event-list.component';
+import { EventComponent } from './pages/event/event.component';
+import { RegisterFormComponent } from './pages/register-form/register-form.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
-	{ path: "register", component: RegisterFormComponent },
-	{ path: "events", component: EventListComponent },
-	{ path: "events/:name", component: EventComponent },
-	{ path: "", component: HomeComponent },
-	{ path: "**", redirectTo: "" }
+	{ path: 'register', component: RegisterFormComponent },
+	{ path: 'contact', component: ContactComponent },
+	{ path: 'events', component: EventListComponent },
+	{ path: 'events/:name', component: EventComponent },
+	{ path: '', component: HomeComponent },
+	{ path: '**', redirectTo: '' }
 ];
 
 @NgModule({
