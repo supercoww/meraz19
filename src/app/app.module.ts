db9256cee66040618941a95117923e1445a471c1
supercoww <ambar.ytl@gmail.com>
2019-09-11 12:50:05 +0530

    add contact info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ParticlesModule } from 'angular-particle';
 
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -10,16 +20,9 @@ import { HeroComponent } from './hero/hero.component';
 import { TimerComponent } from './timer/timer.component';
 import { FooterComponent } from './footer/footer.component';
 import { EventListComponent } from './pages/event-list/event-list.component';
-import { CoreModule } from './core/core.module';
-import { ParticlesModule } from 'angular-particle';
 import { EventComponent } from './pages/event/event.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterFormComponent } from './pages/register-form/register-form.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
@@ -45,7 +48,8 @@ import { ContactComponent } from './pages/contact/contact.component';
 		NoopAnimationsModule,
 		MatButtonModule,
 		MatInputModule,
-		BrowserAnimationsModule
+		BrowserAnimationsModule,
+		MatIconModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
