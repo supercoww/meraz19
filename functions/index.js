9c6109da081bf6509af1b93351dd4c8cffc40c45
KShivendu <shivendu@iitbhilai.ac.in>
2019-09-27 20:50:02 +0530

    Backup before chaning borders

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,7 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 let db = admin.firestore();
 //const http = require('http');
+
 //const https = require('https');
 //const checksum_lib = require('checksum.js');
 
@@ -59,11 +60,15 @@ app.get('/readDB', async (req, res) => {
 		});
 });
 
+app.get('/api/paytm/', async (req, res) => {
+
+});
+
 app.get('/api/pay/', async (req, res) => {
 	console.log('API/PAY is being accessed');
 	//   res.status(200).json({head:"Something",mess:"Message"});
 
-	let testRef = db.collection('registered').doc('test');
+	let testRef = db.collection('registered').doc('test2');
 	let response_Data = testRef.set({
 		//   PAYTMParams: params,
 		name: 'NAME',
@@ -74,9 +79,14 @@ app.get('/api/pay/', async (req, res) => {
 		address2: '3123123121',
 		college: 'IIT Bhilai',
 		comment: 'Some comment'
+	}).then(()=>{
+		console.log("Successfully added");
+		return 0;
+	}).catch((err)=>{
+		console.log("Error in adding to database: ",err);
 	});
-	res.status(200).json(response_Data);
-	console.log(response_Data);
+
+	res.status(200).json({"name":"Uou"});
 });
 
 exports.helloWorld = functions.https.onRequest(app);
@@ -88,6 +98,87 @@ var PaytmConfig = {
 };
 const qs = require('querystring');
 const checksum_lib = require('./checksum.js');
+
+app.post('/api/redirect2/', async (req, res) => {
+	console.log(req.body.email);
+	console.log(JSON.stringify(req.body));
+	// res.send(JSON.stringify(req.body));
+	
+	var ID = new Date().getTime();
+	var params = {};
+	params['MID'] = PaytmConfig.mid;
+	params['WEBSITE'] = PaytmConfig.website;
+	params['CHANNEL_ID'] = 'WEB';
+	params['INDUSTRY_TYPE_ID'] = 'Retail';
+	params['ORDER_ID'] = 'TEST_' + ID;
+	params['CUST_ID'] = 'Entry_' + ID;
+	params['TXN_AMOUNT'] = '1000';
+	params['CALLBACK_URL'] ='https://us-central1-meraz-d165d.cloudfunctions.net/helloWorld/api/payDone';
+		// 'http://localhost:5000/meraz-d165d/us-central1/helloWorld/api/payDone';
+	//
+
+	//'http://localhost:'+port+'/callback';
+
+	params['EMAIL'] = req.body.email;
+	params['MOBILE_NO'] = req.body.phone;
+
+	let testRef = db.collection('registered').doc(params['CUST_ID']);
+	const { name ,email,phone,whatsapp,address1,address2,college,comment} = req.body;
+	let datFire = {
+		normu : "Some text",
+		reg_name: name,
+		reg_email: email,
+		reg_phone: phone,
+		reg_whatsapp: whatsapp,
+		reg_address1: address1,
+		reg_address2:address2,
+		reg_college: college,
+		reg_comment: comment
+	};
+	
+	console.log("Printing database values  \n",datFire);
+	let response_Data = testRef.set(datFire).then(()=>{
+		console.log("Successfully added");
+		return 0;
+	}).catch((err)=>{
+		console.log("Error in adding to database: ",err);
+	});
+
+	checksum_lib.genchecksum(params, PaytmConfig.key, (err, checksum) => {
+		//console.log("Error can be :"+err);
+		if (err) res.send('Some Error Occured');
+		var txn_url =
+			'https://securegw-stage.paytm.in/theia/processTransaction'; // for staging
+		// var txn_url = "https://securegw.paytm.in/theia/processTransaction"; // for production
+
+		var form_fields = '';
+		for (var x in params) {
+			form_fields +=
+				"<input type='hidden' name='" +
+				x +
+				"' value='" +
+				params[x] +
+				"' >";
+		}
+		form_fields +=
+			"<input type='hidden' name='CHECKSUMHASH' value='" +
+			checksum +
+			"' >";
+		res.type('.html');
+		res.send(
+			'<html><head><title>Merchant Checkout Page</title></head><body><center><h1>Please do not refresh this page...</h1></center><form method="post" action="' +
+				txn_url +
+				'" name="f1">' +
+				form_fields +
+				'</form><script type="text/javascript">document.f1.submit();</script></body></html>'
+		);
+	});
+});
+
+app.get('/api/redirect2/', async (req, res) => {
+	// res.send("Some");
+	res.send(("GET"));
+});
 
 app.get('/api/redirect/', async (req, res) => {
 	var params = {};
@@ -163,7 +254,7 @@ app.post('/api/payDone', async (req, res) => {
 	}
 	html += '<br/><br/>';
 
-	// verify the checksum
+	// verify the checksumb
 	var checksumhash = post_data.CHECKSUMHASH;
 	// delete post_data.CHECKSUMHASH;
 	var result = checksum_lib.verifychecksum(
