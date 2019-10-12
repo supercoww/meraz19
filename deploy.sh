fa541d55a73fab657a45bc925284bc37ff38b19f
KShivendu <shivendu@iitbhilai.ac.in>
2019-10-12 17:21:55 +0530

    Just some changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-# ng build --prod
+ng build --prod
 zip dist/meraz19.zip -r dist/meraz19/
 scp dist/meraz19.zip yl643coddvja@meraz.in:public_html/meraz19.zip
 ssh yl643coddvja@meraz.in 'public_html/deploy.sh'
