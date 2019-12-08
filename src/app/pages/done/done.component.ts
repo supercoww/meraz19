2a3c208cec10ae81644250a50ed4bf91b5b25c28
Ambar Mutha <ambar.ytl@gmail.com>
2019-12-08 20:25:18 +0530

    add gh-pages

--- src/app/pages/done/done.component.ts
+++ src/app/pages/done/done.component.ts
@@ -2,25 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-done',
-  templateUrl: './done.component.html',
-  styleUrls: ['./done.component.scss']
+	selector: 'app-done',
+	templateUrl: './done.component.html',
+	styleUrls: ['./done.component.scss']
 })
 export class DoneComponent implements OnInit {
-status: string;
-private sub: any;
+	status: string;
+	private sub: any;
 
-constructor(private route: ActivatedRoute) { }
+	constructor(private route: ActivatedRoute) {}
 
-ngOnInit() {
-	this.sub = this.route.params.subscribe(params => {
-		this.status = params['status'];
-	});
+	ngOnInit() {
+		this.sub = this.route.params.subscribe(params => {
+			this.status = params['status'];
+		});
+	}
 
-}
-
-ngOnDestroy() {
-	this.sub.unsubscribe();
-}
-
+	ngOnDestroy() {
+		this.sub.unsubscribe();
+	}
 }
